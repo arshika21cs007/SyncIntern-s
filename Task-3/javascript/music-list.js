2227f52493dee72d5fe2cdc2cd3dfd19604fa04f
arshika21cs007 <110716883+arshika21cs007@users.noreply.github.com>
2023-08-07 15:38:57 +0530

    Add files via upload

---
+++ Task-3/javascript/music-list.js
@@ -0,0 +1,33 @@
+let allMusic = [
+    {
+      name: "The nights",
+      artist: "Avicii",
+      img: "The Nights",
+      src: "The Nights"
+    },
+    {
+      name: "Titanium",
+      artist: "David Guetta",
+      img: "Titanium",
+      src: "Titanium"
+    },
+    {
+      name: "See You again",
+      artist: "charlie puth",
+      img: "See You Again",
+      src: "See You Again"
+    },
+    {
+      name: "Audience choice mashup 2020",
+      artist: "Nithyashree",
+      img: "Mashup",
+      src: "Mashup"
+    },
+    {
+      name: "Kuttanadan Punjayile",
+      artist: "Vidya Vox",
+      img: "Vidhya vox",
+      src: "Vidhya vox"
+    },
+    
+  ];
